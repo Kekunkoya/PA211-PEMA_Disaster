58e90d0128d9ad4af557f18f8ca106713546328f
Kekunkoya <91146674+Kekunkoya@users.noreply.github.com>
2025-08-01 12:29:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-import streamlit as st
+st.set_page_config(
+    page_title="PA 211_PEMA RAG Demo",  # Text in the browser tab
+    page_icon="🏠",               # Emoji or path to an icon file
+    layout="wide"                 # Optional: "centered" or "wide"
+)
+ streamlit as st
 import os
 import openai
 import google.generativeai as genai
